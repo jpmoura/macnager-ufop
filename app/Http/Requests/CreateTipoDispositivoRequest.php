834b3ad19b0cb89e2b11aa629aef11452f870d61
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-01-28 17:15:35 -0200

    Refatoração de rotas e métodos envolvendo o modelo TipoDispositivo

---
+++ app/Http/Requests/CreateTipoDispositivoRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTipoDispositivoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'descricao' => 'required|max:50|unique:tipo_dispositivo,descricao',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'descricao.required' => 'O campo Descrição é obrigatório',
+            'descricao.max' => 'A descrição pode conter no máximo :max caracteres',
+            'descricao.unique' => 'Essa descrição já está sendo usada',
+        ];
+    }
+}
