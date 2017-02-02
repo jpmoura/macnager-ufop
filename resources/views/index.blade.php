ba002b361eb579a9dcae625f0661dfa1311154aa
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-02-02 03:19:01 -0200

    Adicionado as chamadas de construção dos gráficos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,44 +9,37 @@
 @endsection
 
 @section('content')
-    {!! HTML::script('https://www.gstatic.com/charts/loader.js') !!}
+    {!! HTML::script('public/js/plugins/chartjs/Chart.min.js') !!}
 
-    <div class='col-lg-12'>
+    <div class='col-md-12'>
         @can('administrate')
-            <div class="row">
-                <div class="box box-primary-ufop">
-                    <div class="box-body">
-                        <div class="row">
-                            <h3 class="text-center">Taxa de ocupação das faixas</h3>
-                        </div>
-
-                        <div class="row">
-                            @for($i=0, $f=152; $i < count($faixa) ; $i++, $f++)
-                                <div class="col-lg-6 text-center">
-                                    <div id="donutchart{{$f}}"></div>
-                                    <script type="text/javascript">
-                                        google.charts.load("current", {packages:["corechart"]});
-                                        google.charts.setOnLoadCallback(drawChart);
-                                        function drawChart() {
-                                            var data = google.visualization.arrayToDataTable([
-                                                ['Faixa', 'Usado (%)'],
-                                                ['Em uso', {{$faixa[$f]}}],
-                                                ['Livre', {{255 - $faixa[$f]}}]
-                                            ]);
-
-                                            var options = {
-                                                title: 'Alocação da Faixa {{$f}}',
-                                                pieHole: 0.4,
-                                            };
-
-                                            var chart = new google.visualization.PieChart(document.getElementById('donutchart{{$f}}'));
-                                            chart.draw(data, options);
-                                        }
-                                    </script>
-                                </div>
-                            @endfor
-                        </div>
+            <div class="box box-primary-ufop">
+                <div class="text-center">
+                    <h3>Resumo Estatístico da Rede</h3>
+                </div>
+                <div class="box-body">
+                    <div class="col-md-4">
+                        <canvas id="devicesPerUserType">
+                            {!! $charts['devicesPerUserType']->render() !!}
+                        </canvas>
                     </div>
+                    <div class="col-md-4">
+                        <canvas id="devicesPerType">
+                            {!! $charts['devicesPerType']->render() !!}
+                        </canvas>
+                    </div>
+                    <div class="col-md-4">
+                        <canvas id="requestsPerStatus">
+                            {!! $charts['requestsPerStatus']->render() !!}
+                        </canvas>
+                    </div>
+                    @foreach($subredes as $subrede)
+                        <div class="col-md-3">
+                            <canvas id="subrede{{$subrede->id}}">
+                                {!! $charts['subrede' . $subrede->id]->render() !!}
+                            </canvas>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         @endcan
