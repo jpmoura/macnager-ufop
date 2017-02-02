a6fd4c7db236b6d19a710ae6b06fe2bdcb6bccb4
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2017-02-02 03:20:29 -0200

    Criados método para criação de gráficos

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,14 +2,161 @@
 
 namespace App\Http\Controllers;
 
+use App\Subrede;
 use Illuminate\Http\Request;
 
-use Auth;
 use App\Http\Requests;
 use App\Requisicao;
 
 class PagesController extends Controller
 {
+    private function rand_color() {
+        return '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
+    }
+
+    /**
+     * Recupera todos os IPs livres de uma subrede.
+     * @param Subrede $subrede Instância da subrede que terá os IPs recuperados
+     * @return array Array contendo todos os IPs
+     */
+    public function getAvailableIps(Subrede $subrede)
+    {
+
+        $allSubnetIps = $subrede->getAllIps(); // Obtém todos os IPs da subrede
+        $activeIpsRaw = Requisicao::select('ip')->where('status', 1)->orWhere('status', 4)->get(); // Obtém todos os IPs sendo usados
+
+        // Transforma a coleção resultante da query em um array contendo somento os IPs
+        $activeIps = array();
+        foreach ($activeIpsRaw as $rawIP) $activeIps[] = $rawIP->ip;
+
+        // Verifica quais IP da subrede não estão sendo utilizados
+        $unusedIPs = array();
+        foreach ($allSubnetIps as $subnetIp) {
+            if (!in_array($subnetIp, $activeIps)) $unusedIPs[] = $subnetIp; // Se o IP não estiver na lista de ativos, ent"ao adiciona da lista de IPs não utilizados
+        }
+
+        return $unusedIPs;
+    }
+
+    private function generateDevicesPerUserTypeChart($requisicoesAtivas)
+    {
+        // Agrupa todos os tipos de usuários com requisições ativas
+        $todosTiposUsuarios = $requisicoesAtivas->groupBy('tipoDoUsuario.descricao');
+
+        $tipoUsuario = array();
+        $tipoUsuarioCores = array();
+        foreach($todosTiposUsuarios->keys() as $key)
+        {
+            $tipoUsuario[$key] = $todosTiposUsuarios[$key]->count();
+            $tipoUsuarioCores[] = $this->rand_color();
+        }
+
+        $chart = app()->chartjs
+            ->name('devicesPerUserType')
+            ->type('doughnut')
+            ->element('devicesPerUserType')
+            ->labels(array_keys($tipoUsuario))
+            ->datasets([
+                [
+                    'backgroundColor' => $tipoUsuarioCores,
+                    'data' => array_values($tipoUsuario)
+                ]
+            ])
+            ->options([
+                'title' => ['display' => true, 'text' => 'Quantidades de Dipositivos por Tipo de Usuário'],
+            ]);
+
+        return $chart;
+    }
+
+    private function generateDevicesPerTypeChart($requisicoesAtivas)
+    {
+        // Agrupa todos os tipos de usuários com requisições ativas
+        $todosTipos = $requisicoesAtivas->groupBy('tipoDoDispositivo.descricao');
+
+        $tipos = array();
+        $tiposCores = array();
+        foreach($todosTipos->keys() as $key)
+        {
+            $tipos[$key] = $todosTipos[$key]->count();
+            $tiposCores[] = $this->rand_color();
+        }
+
+        $chart = app()->chartjs
+            ->name('devicesPerType')
+            ->type('doughnut')
+            ->element('devicesPerType')
+            ->labels(array_keys($tipos))
+            ->datasets([
+                [
+                    'backgroundColor' => $tiposCores,
+                    'data' => array_values($tipos)
+                ]
+            ])
+            ->options([
+                'title' => ['display' => true, 'text' => 'Quantidades de Dipositivos por Tipo'],
+            ]);
+
+        return $chart;
+    }
+
+    private function generateRequestsPerStatusChart()
+    {
+        $requisicoes = Requisicao::with('tipoStatus')->get();
+
+        // Agrupa as requisicoes por tipo
+        $todosTipos = $requisicoes->groupBy('tipoStatus.descricao');
+
+        $tipos = array();
+        $tiposCores = array();
+        foreach($todosTipos->keys() as $key)
+        {
+            $tipos[$key] = $todosTipos[$key]->count();
+            $tiposCores[] = $this->rand_color();
+        }
+
+        $chart = app()->chartjs
+            ->name('requestsPerStatus')
+            ->type('doughnut')
+            ->element('requestsPerStatus')
+            ->labels(array_keys($tipos))
+            ->datasets([
+                [
+                    'backgroundColor' => $tiposCores,
+                    'data' => array_values($tipos)
+                ]
+            ])
+            ->options([
+                'title' => ['display' => true, 'text' => 'Quantidades de Requisições por Status'],
+            ]);
+
+        return $chart;
+    }
+
+
+    private function generateSubnetUsageChart(Subrede $subrede)
+    {
+        $ipsLivres = count($this->getAvailableIps($subrede));
+        $todosIps = count($subrede->getAllIps());
+
+        $chart = app()->chartjs
+            ->name('subrede' . $subrede->id)
+            ->type('doughnut')
+            ->element('subrede' . $subrede->id)
+            ->labels(['Livre', 'Em Uso'])
+            ->datasets([
+                [
+                    'backgroundColor' => ['#36A2EB', '#FF6384'],
+                    'data' => [$ipsLivres, $todosIps - $ipsLivres]
+                ]
+            ])
+            ->options([
+                'title' => ['display' => true, 'text' => 'Utilização Rede ' . $subrede->tipo->descricao . ' - ' . $subrede->descricao],
+            ]);
+
+        return $chart;
+    }
+
     /**
      * Recupera informações para serem mostradas na página inicial dependendo do nível do usuário
      * @return mixed View com dados sumarizados
@@ -17,19 +164,22 @@ class PagesController extends Controller
     public function home()
     {
         // Se for um administrador
-        if(Auth::user()->isAdmin())
+        if(auth()->user()->isAdmin())
         {
-            // Recupera a quantidade de dispositivos alocados em cada faixa
-            for ($faixa=152; $faixa < 156; $faixa++)
-            {
-                $count = Requisicao::where('ip', 'like', '200.239.' . $faixa . '.%')->where('status', 1)->count();
-                $used[$faixa] = $count;
-            }
+            $requisicoesAtivas = Requisicao::with('tipoDoDispositivo', 'tipoDoUsuario')->where('status', 1)->get();
+
+            $charts['devicesPerUserType'] = $this->generateDevicesPerUserTypeChart($requisicoesAtivas);
+            $charts['devicesPerType'] = $this->generateDevicesPerTypeChart($requisicoesAtivas);
+            $charts['requestsPerStatus'] = $this->generateRequestsPerStatusChart();
+
+            // Gerando gráficos das subredes
+            $subredes = Subrede::with('tipo')->get();
+            foreach($subredes as $subrede) $charts['subrede' . $subrede->id] = $this->generateSubnetUsageChart($subrede);
 
             // Recupera a quantidade de requisições que ainda não foram julgadas
             session()->put('novosPedidos', Requisicao::where('status', '=', 0)->count());
 
-            return view('index')->with(['faixa' => $used]);
+            return view('index', compact('charts'))->with(['subredes' => $subredes]);
         }
         else
         {
