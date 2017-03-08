1fb25fd72054418ee260dcb4a95515c00f3a8424
jpmoura <moura.joaopedro@gmail.com>
2017-03-08 13:49:16 -0300

    Correção da importação da classe Log

--- app/Listeners/LogNewUserCreated.php
+++ app/Listeners/LogNewUserCreated.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\NewUserCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
 
 class LogNewUserCreated
 {
